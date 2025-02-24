9b44ec09cb5c52baceb465041a334b38e3e3e1e7
Fernando-Pucha <fernandopuchap@gmail.com>
2025-02-24 16:03:19 +0000

    Done rutas, cors dia 1
    Co-authored-by: Patricia-GraciaGutierrez <Patricia-GraciaGutierrez@users.noreply.github.com>
    Co-authored-by: Aytha <aythp@users.noreply.github.com>
    Co-authored-by: RobertFM89 <RobertFM89@users.noreply.github.com>
    Co-authored-by: Kilian D. Hernández Chirino <Kilianhc@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
+import express, { json, urlencoded } from "express";
+import morgan from "morgan";
+import cors from 'cors' 
+import cookieParser from "cookie-parser";
 const PORT = 5005;
+import cohortsData from "./cohorts.json" assert { type: 'json' };
+import studentsData from './students.json'assert { type: 'json' };
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -15,20 +18,38 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-app.use(express.json());
+app.use(json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5005'], // Add the URLs of allowed origins to this array
+  })
+);
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohortsData)
+})
+
+app.get("/api/students", (req, res) => {
+  res.json(studentsData)
+})
+
+/* app.use('/*', (req, res)=> {
+  res.status(404).sendFile(__dirname + '/views/not-found.html')
+  }) */
 
 // START SERVER
 app.listen(PORT, () => {
